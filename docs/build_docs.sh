dfe1ac2ee6eb0c77880f3cf58436e3dfa5736c8b
RussellGarwood <russell.garwood@manchester.ac.uk>
2025-02-10 22:37:24 +0100

    update installation instructions for sphinx theme

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -2,7 +2,8 @@
 #On Linux that erquires these commands (assuming Python3 is already installed):
 #sudo apt-get install python3-sphinx
 #sudo apt install python3-pip
-#sudo pip3 install sphinx_rtd_theme
+#Before 24.04 - sudo pip3 install sphinx_rtd_theme
+#24.04 and after: sudo apt-get -y install python3-sphinx-rtd-theme
 echo "Now building trevosim doc"
 sphinx-build ./ ../docs_build
 
