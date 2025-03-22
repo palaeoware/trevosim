8a414d4e8dc561b2e02486e8997a315e994cef8d
RussellGarwood <russell.garwood@manchester.ac.uk>
2025-03-22 17:13:04 +0100

    New version number 3.0.0 --> 3.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ copyright = '(TREvoSim documentation) 2024, Russell J. Garwood, Alan R.T. Spence
 author = 'Russell J. Garwood, Alan R.T. Spencer'
 
 # The short X.Y version
-version = '3.0.0'
+version = '3.1.0'
 # The full version, including alpha/beta/rc tags
-release = '3.0.0'
+release = '3.1.0'
 
 
 # -- General configuration ---------------------------------------------------
